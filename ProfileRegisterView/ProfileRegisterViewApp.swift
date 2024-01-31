aab65a63db5d06fecfa741b9d1025055d3345e10
심상갑 <kappa2410@gmail.com>
2024-01-31 09:54:52 +0900

    Initial Commit

---
+++ ProfileRegisterView/ProfileRegisterViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileRegisterViewApp.swift
+//  ProfileRegisterView
+//
+//  Created by Gab on 2024/01/31.
+//
+
+import SwiftUI
+
+@main
+struct ProfileRegisterViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
